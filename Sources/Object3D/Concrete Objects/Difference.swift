7fea2b8bb22bf52d3dc0455a8984a186bd2c83fa
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2018-07-10 01:03:58 -0700

    Created `public struct Difference: Object3D`; Added gettable and settable property `public var position: Position3D` to RawObject3D;

---
+++ Sources/Object3D/Concrete Objects/Difference.swift
@@ -0,0 +1,28 @@
+//
+//  Difference.swift
+//  Geometry3DValueTypes
+//
+//  Created by Jeremy Bannister on 7/9/18.
+//
+
+import Geometry3DValueTypes
+
+// MARK: - --> Initial Declaration <--
+public struct Difference: Object3D {
+  public var position: Position3D
+  public var original: Object3D
+  public var subtractions: [Object3D]
+  
+  public init (position: Position3D, original: Object3D, subtractions: [Object3D]) {
+    self.position = position
+    self.original = original
+    self.subtractions = subtractions
+  }
+}
+
+// MARK: - Conversion to RawObject3D
+extension Difference {
+  public var asRawObject3D: RawObject3D {
+    return .difference(position: position, original: original.asRawObject3D, subtractions: subtractions.mapToRawObject3D)
+  }
+}
--- Sources/Object3D/Raw Object3D/RawObject3D.swift
+++ Sources/Object3D/Raw Object3D/RawObject3D.swift
@@ -20,3 +20,29 @@ public indirect enum RawObject3D {
 extension RawObject3D {
   public static let empty: RawObject3D = .union(position: .zero, rawObjects: [])
 }
+
+extension RawObject3D {
+  public func withPosition (_ newValue: Position3D) -> RawObject3D {
+    var copy = self
+    copy.position = newValue
+    return copy
+  }
+  public var position: Position3D {
+    get {
+      switch self {
+      case .cube(position: let position, size: _): return position
+      case .cylinder(position: let position, lowerRadius: _, upperRadius: _, depth: _): return position
+      case .union(position: let position, rawObjects: _): return position
+      case .difference(position: let position, original: _, subtractions: _): return position
+      }
+    }
+    set {
+      switch self {
+      case .cube(position: _, size: let size): self = .cube(position: newValue, size: size)
+      case .cylinder(position: _, lowerRadius: let lowerRadius, upperRadius: let upperRadius, depth: let depth): self = .cylinder(position: newValue, lowerRadius: lowerRadius, upperRadius: upperRadius, depth: depth)
+      case .union(position: _, rawObjects: let rawObjects): self = .union(position: newValue, rawObjects: rawObjects)
+      case .difference(position: _, original: let original, subtractions: let subtractions): self = .difference(position: newValue, original: original, subtractions: subtractions)
+      }
+    }
+  }
+}
